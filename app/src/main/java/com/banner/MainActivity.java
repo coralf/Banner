1188b5eec005b2004530963819c2aa1ef315d00e
thinking <861958334@qq.com>
2017-05-28 21:03:43 +0800

    banner

---
+++ app/src/main/java/com/banner/MainActivity.java
@@ -0,0 +1,39 @@
+package com.banner;
+
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.banner.widget.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private int[] imgs = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        List<View> itemViews = new ArrayList<View>();
+        for (int i = 0; i < imgs.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(imgs[i]);
+            itemViews.add(imageView);
+        }
+        Banner banner1 = new Banner(this, itemViews);
+        banner1.setBackgroundColor(Color.parseColor("#3F51B5"));
+    }
+}
